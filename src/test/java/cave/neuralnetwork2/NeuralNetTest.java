33d541a55408e965e5d49cec9852d63853a22241
Jeremy Stocks <jeremy.stocks@zoho.com>
2025-07-24 23:16:28 +0200

    03010_Neural_Net_Test_Class

---
+++ src/test/java/cave/neuralnetwork2/NeuralNetTest.java
@@ -0,0 +1,27 @@
+package cave.neuralnetwork2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import cave.matrix.Matrix;
+
+public class NeuralNetTest {
+
+	@Test
+	public void test() {
+		
+		Matrix input = new Matrix(3, 1, i->(i+1));
+		Matrix weights = new Matrix(3, 3, i->(i+1));
+		Matrix biases = new Matrix(3, 1, i->(i+1));
+		
+		Matrix result = weights.multiply(input);
+		
+		System.out.println(input);
+		System.out.println(weights);
+		System.out.println(biases);
+		System.out.println(result);
+		
+	}
+
+}
