7f992286c1920b9d441b867c9b681af92585accf
Jeremy Stocks <jeremy.stocks@zoho.com>
2025-07-25 23:33:18 +0200

    03060_A_ReLu_Test

--- src/test/java/cave/neuralnetwork2/NeuralNetTest.java
+++ src/test/java/cave/neuralnetwork2/NeuralNetTest.java
@@ -2,26 +2,53 @@ package cave.neuralnetwork2;
 
 import static org.junit.Assert.*;
 
+import java.util.Random;
+
 import org.junit.Test;
 
 import cave.matrix.Matrix;
 
 public class NeuralNetTest {
+	private Random random = new Random();
 
 	@Test
-	public void test() {
+	public void testAddBias() {
+
+		Matrix input = new Matrix(3, 3, i -> (i + 1));
+		Matrix weights = new Matrix(3, 3, i -> (i + 1));
+		Matrix biases = new Matrix(3, 1, i -> (i + 1));
+
+		Matrix result = weights.multiply(input).modify((row, col, value) -> value + biases.get(row));
+
+		double[] expectedValues = { +31.00000, +37.00000, +43.00000, +68.00000, +83.00000, +98.00000, +105.00000,
+				+129.00000, +153.00000 };
+
+		Matrix expected = new Matrix(3, 3, i->expectedValues[i]);
 		
-		Matrix input = new Matrix(3, 1, i->(i+1));
-		Matrix weights = new Matrix(3, 3, i->(i+1));
-		Matrix biases = new Matrix(3, 1, i->(i+1));
+		assertTrue(expected.equals(result));
+	}
+
+	@Test
+	public void testRelu() {
 		
-		Matrix result = weights.multiply(input).modify((row, col, value)->value + biases.get(row));
-		
+		final int numberNeurons = 5;
+		final int numberInputs = 6;
+		final int inputSize = 4;
+
+		Matrix input = new Matrix(inputSize, numberInputs, i -> random.nextDouble());
+		Matrix weights = new Matrix(numberNeurons, inputSize, i -> random.nextGaussian());
+		Matrix biases = new Matrix(numberNeurons, 1, i -> random.nextGaussian());
+
+		Matrix result = weights.multiply(input).modify((row, col, value) -> value + biases.get(row));
+
 		System.out.println(input);
 		System.out.println(weights);
 		System.out.println(biases);
 		System.out.println(result);
 		
+		
 	}
-
+	
+	
+	
 }
