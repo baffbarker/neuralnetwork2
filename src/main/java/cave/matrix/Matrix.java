e46bebda57bd4ab385cdc74701af0ae7e044e304
Jeremy Stocks <jeremy.stocks@zoho.com>
2025-08-01 23:10:27 +0200

    03150_Deciding_Weight_Matrix_Sizes

--- src/main/java/cave/matrix/Matrix.java
+++ src/main/java/cave/matrix/Matrix.java
@@ -52,6 +52,15 @@ public class Matrix {
 		}
 	}
 
+	public int getRows() {
+		return rows;
+	}
+	
+	public int getCols() {
+		return cols;
+	}
+	
+	
 	public Matrix apply(IndexValueProducer producer) {
 		Matrix result = new Matrix(rows, cols);
 
