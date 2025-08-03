6d4fb89ebf88acecc5c1d501edaa659d2a95a282
Jeremy Stocks <jeremy.stocks@zoho.com>
2025-08-03 23:06:03 +0200

    03170_Configuring_Dense_Layers

--- src/main/java/cave/neuralnetwork2/Engine.java
+++ src/main/java/cave/neuralnetwork2/Engine.java
@@ -1,11 +1,29 @@
 package cave.neuralnetwork2;
 
 import java.util.LinkedList;
+import java.util.Random;
+
+import cave.matrix.Matrix;
 
 public class Engine {
 	private LinkedList<Transform> transforms = new LinkedList<>();
+	private LinkedList<Matrix> weights = new LinkedList<>();
+	private LinkedList<Matrix> biases = new LinkedList<>();
 	
-	public void add(Transform transform) {
+	public void add(Transform transform, double... params) {
+		
+		Random random = new Random();
+		
+		if(transform == Transform.DENSE) {
+			int numberNeurons = (int)params[0];
+			int weightsPerNeuron = weights.size() == 0 ? (int)params[1]: weights.getLast().getRows();
+			
+			Matrix weight = new Matrix(numberNeurons, weightsPerNeuron, i->random.nextGaussian());
+			Matrix bias = new Matrix(numberNeurons, 1, i->random.nextGaussian());
+			
+			weights.add(weight);
+			biases.add(bias);
+		}
 		transforms.add(transform);
 	}
 	
