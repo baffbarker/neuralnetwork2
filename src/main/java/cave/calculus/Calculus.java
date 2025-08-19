26760092dfde2ed3bf26fe7e15ecd7e32f5f5651
Jeremy Stocks <jeremy.stocks@zoho.com>
2025-08-19 23:07:22 +0200

    05220_Programming_the_Chain_Rule

--- src/main/java/cave/calculus/Calculus.java
+++ src/main/java/cave/calculus/Calculus.java
@@ -3,29 +3,51 @@ package cave.calculus;
 import java.util.function.DoubleFunction;
 
 public class Calculus {
-	
+
 	private static final double INC = 0.00001;
 
 	public static double func1(double x) {
 		return 3.7 * x + 5.3;
 	}
-	
+
 	public static double func2(double x) {
-		return x * x;
+		return x * x - 3.23;
 	}
 	
+	public static double func3(double x) {
+		return func2(func1(x));
+	}
+
 	public static double differentiate(DoubleFunction<Double> func, double x) {
-		
+
 		double output1 = func.apply(x);
 		double output2 = func.apply(x + INC);
-		
-		return (output2 - output1)/INC;
+
+		return (output2 - output1) / INC;
 	}
+
 	public static void main(String[] args) {
+
+		double x = 3.64;
+		double y = func1(x);
+		double z = func2(y);
+
+		/*
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		System.out.println(func2(func1(x)));
+		System.out.println(func3(x));
+	*/
 		
-		for(double x = -2; x < 2; x += 0.1) {
-			double gradient = differentiate(Calculus::func2, x);
-			System.out.printf("%.2f\t%.2f\n", x, gradient);
-		}
+		double dydx = differentiate(Calculus::func1, x);
+		double dzdy = differentiate(Calculus::func2, y);
+		double dzdx = differentiate(Calculus::func3, x);
+		
+		System.out.println(dydx);
+		System.out.println(dzdy);
+		System.out.println(dzdx);
+		System.out.println(dzdy * dydx);
+		
 	}
 }
