8769a7d4513b0cd010d95e7d508d588e4a555366
Jeremy Stocks <jeremy.stocks@zoho.com>
2025-08-25 22:01:24 +0200

    06030_A_Batch_Result_Class

---
+++ src/main/java/cave/neuralnetwork2/BatchResult.java
@@ -0,0 +1,17 @@
+package cave.neuralnetwork2;
+
+import java.util.LinkedList;
+
+import cave.matrix.Matrix;
+
+public class BatchResult {
+	private LinkedList<Matrix> io = new LinkedList<>();
+	
+	public LinkedList<Matrix> getIo() {
+		return io;
+	}
+	
+	public void addIo(Matrix m) {
+		io.add(m);
+	}
+}
