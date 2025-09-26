e0318b3720af2260aa04566b16781b1de6be0f23
jeremy <wokefinder@protonmail.com>
2025-09-26 23:06:02 +0200

    08110_Finishing_the_Loader_Test

--- src/test/java/cave/neuralnetwork2/loader/test/TestTestLoader.java
+++ src/test/java/cave/neuralnetwork2/loader/test/TestTestLoader.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import cave.matrix.Matrix;
 import cave.neuralnetwork2.loader.BatchData;
 import cave.neuralnetwork2.loader.Loader;
 import cave.neuralnetwork2.loader.MetaData;
@@ -13,20 +14,42 @@ public class TestTestLoader {
 	@Test
 	public void test() {
 
-		int batchSize = 32;
+		int batchSize = 33;
 		
-		Loader testLoader = new TestLoader(60_000, 32);
+		Loader testLoader = new TestLoader(600, batchSize);
 		
 		MetaData metaData = testLoader.open();
 		
-		for(int i= 0; i < metaData.getNumberBatches(); i++) {
+		int numberItems = metaData.getNumberItems();
+		
+		int lastBatchSize = numberItems % batchSize;
+		
+		int numberBatches = metaData.getNumberBatches();
+		
+		for(int i= 0; i < numberBatches; i++) {
 			BatchData batchData = testLoader.readBatch();
 			
 			assertTrue(batchData != null);
 			
 			int itemsRead = metaData.getItemsRead();
 			
-			assertTrue(itemsRead == batchSize);
+			int inputSize = metaData.getInputSize();
+			int expectedSize = metaData.getExpectedSize();
+			
+			
+			Matrix input = new Matrix(inputSize, itemsRead, batchData.getInputBatch());
+			Matrix expected = new Matrix(expectedSize, itemsRead, batchData.getExpectedBatch());
+			
+			assertTrue(input.sum() != 0);
+			assertTrue(expected.sum() == itemsRead);
+			
+			
+			if(i == numberBatches -1) {
+				assertTrue(itemsRead == lastBatchSize);
+			} else {
+				assertTrue(itemsRead == batchSize);
+			}
+			
 		}
 	}
 
