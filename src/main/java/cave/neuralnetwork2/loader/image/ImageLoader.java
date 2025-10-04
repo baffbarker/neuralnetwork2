88a46eddc50f25538e7af5fa71f145ca7d7af296
jeremy <wokefinder@protonmail.com>
2025-10-05 00:14:23 +0200

    09080_Storing_the_MetaData

--- src/main/java/cave/neuralnetwork2/loader/image/ImageLoader.java
+++ src/main/java/cave/neuralnetwork2/loader/image/ImageLoader.java
@@ -3,20 +3,22 @@ package cave.neuralnetwork2.loader.image;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.IllegalFormatWidthException;
 
 import cave.neuralnetwork2.loader.BatchData;
 import cave.neuralnetwork2.loader.Loader;
 import cave.neuralnetwork2.loader.MetaData;
 
-public class ImageLoader implements Loader{
+public class ImageLoader implements Loader {
 	private String imageFileName;
 	private String labelFileName;
 	private int batchSize;
-	
+
 	private DataInputStream dsImages;
 	private DataInputStream dsLabels;
-	
-	
+
+	private ImageMetaData metaData;
+
 	public ImageLoader(String imageFileName, String labelFileName, int batchSize) {
 		this.imageFileName = imageFileName;
 		this.labelFileName = labelFileName;
@@ -24,89 +26,99 @@ public class ImageLoader implements Loader{
 	}
 
 	@Override
-	public MetaData open() {
+	public ImageMetaData open() {
 		try {
 			dsImages = new DataInputStream(new FileInputStream(imageFileName));
+		} catch (Exception e) {
+			throw new LoaderException("Cannot open " + imageFileName, e);
 		}
-		catch(Exception e) {
-			throw new LoaderException("Cannot open " + imageFileName,e);
-		}
-		
+
 		try {
 			dsLabels = new DataInputStream(new FileInputStream(labelFileName));
+		} catch (Exception e) {
+			throw new LoaderException("Cannot open " + labelFileName, e);
 		}
-		catch(Exception e) {
-			throw new LoaderException("Cannot open " + labelFileName,e);
-		}
-		
-		readMetaData();
-		return null;
+
+		metaData = readMetaData();
+		return metaData;
 	}
-	
-	private MetaData readMetaData() {
-		
+
+	private ImageMetaData readMetaData() {
+
+		metaData = new ImageMetaData();
+
 		int numberItems = 0;
-		
+
 		try {
 			int magicLabelNumber = dsLabels.readInt();
-			
-			if(magicLabelNumber != 2049) {
+
+			if (magicLabelNumber != 2049) {
 				throw new LoaderException("Label file " + labelFileName + " has wrong format. ");
 			}
-			
+
 			numberItems = dsLabels.readInt();
-			
+
+			metaData.setNumberItems(numberItems);
+
 		} catch (IOException e) {
 			throw new LoaderException("Unable to read " + labelFileName, e);
 		}
-		
+
 		try {
 			int magicImageNumber = dsImages.readInt();
-			
-			if(magicImageNumber != 2051) {
+
+			if (magicImageNumber != 2051) {
 				throw new LoaderException("Image file " + imageFileName + " has wrong format. ");
 			}
-			
-			if(dsImages.readInt() != numberItems) {
-				throw new LoaderException("Image file " + imageFileName + " has different number ofitems to " + labelFileName);
+
+			if (dsImages.readInt() != numberItems) {
+				throw new LoaderException(
+						"Image file " + imageFileName + " has different number ofitems to " + labelFileName);
 			}
-			
+
 			int height = dsImages.readInt();
 			int width = dsImages.readInt();
-			
+
+			metaData.setHeight(height);
+			metaData.setWidth(width);
+
+			metaData.setInputSize(width * height);
+
 			System.out.println(height + " , " + width);
-			
+
 		} catch (IOException e) {
 			throw new LoaderException("Unable to read " + imageFileName, e);
 		}
-		
-		
-		return null;
-		
+
+		metaData.setExpectedSize(10);
+		metaData.setNumberBatches((int) (Math.ceil((double) numberItems) / batchSize));
+
+		return metaData;
+
 	}
 
 	@Override
 	public void close() {
+
+		metaData = null;
+
 		try {
 			dsImages.close();
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			throw new LoaderException("Cannot close " + imageFileName, e);
 		}
-		
+
 		try {
 			dsLabels.close();
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			throw new LoaderException("Cannot close " + labelFileName, e);
 		}
-		
+
 	}
 
 	@Override
-	public MetaData getMetaData() {
-		// TODO Auto-generated method stub
-		return null;
+	public ImageMetaData getMetaData() {
+		return metaData;
 	}
 
 	@Override
@@ -114,6 +126,5 @@ public class ImageLoader implements Loader{
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
-	
+
 }
