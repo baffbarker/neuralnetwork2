b50383233b1f43321cd6b4e5a2e45777354afc5c
jeremy <wokefinder@protonmail.com>
2025-10-05 23:41:03 +0200

    09100_Reading_Image_Data

--- src/main/java/cave/neuralnetwork2/loader/image/ImageLoader.java
+++ src/main/java/cave/neuralnetwork2/loader/image/ImageLoader.java
@@ -20,7 +20,7 @@ public class ImageLoader implements Loader {
 	private DataInputStream dsLabels;
 
 	private ImageMetaData metaData;
-	
+
 	private Lock readLock = new ReentrantLock();
 
 	public ImageLoader(String imageFileName, String labelFileName, int batchSize) {
@@ -88,7 +88,6 @@ public class ImageLoader implements Loader {
 
 			metaData.setInputSize(width * height);
 
-
 		} catch (IOException e) {
 			throw new LoaderException("Unable to read " + imageFileName, e);
 		}
@@ -127,26 +126,24 @@ public class ImageLoader implements Loader {
 	@Override
 	public BatchData readBatch() {
 		readLock.lock();
-		
+
 		try {
 			ImageBatchData batchData = new ImageBatchData();
-			
+
 			int inputItemsRead = readInputBatch(batchData);
 			int expectedItemsRead = readExpectedBatch(batchData);
-			
-			
-			if(inputItemsRead != expectedItemsRead) {
+
+			if (inputItemsRead != expectedItemsRead) {
 				throw new LoaderException("Mismatch between images read and labels read.");
 			}
-			
+
 			metaData.setItemsRead(inputItemsRead);
-			
+
 			return batchData;
-		}
-		finally {
+		} finally {
 			readLock.unlock();
 		}
-		
+
 	}
 
 	private int readExpectedBatch(ImageBatchData batchData) {
@@ -155,7 +152,26 @@ public class ImageLoader implements Loader {
 	}
 
 	private int readInputBatch(ImageBatchData batchData) {
-		// TODO Auto-generated method stub
+
+		try {
+			var totalItemsRead = metaData.getTotalItemsRead();
+			var numberItems = metaData.getNumberItems();
+
+			var numberToRead = Math.min(numberItems - totalItemsRead, batchSize);
+
+			var inputSize = metaData.getInputSize();
+			var numberBytesToRead = numberToRead * inputSize;
+
+			byte[] imageData = new byte[numberBytesToRead];
+
+			var numberRead = dsImages.read(imageData, 0, numberBytesToRead);
+			
+			if(numberRead != numberBytesToRead) {
+				throw new LoaderException("Couldn't read sufficient bytes from image data.");
+			}
+		} catch (IOException e) {
+			throw new LoaderException("Error occurred reading image data.", e);
+		}
 		return 0;
 	}
 
