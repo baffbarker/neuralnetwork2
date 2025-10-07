f6ab78536d7c36fe7325b0ff49ff658a56bd5c90
jeremy <wokefinder@protonmail.com>
2025-10-07 23:16:47 +0200

    09140_Writing_Images

--- src/main/java/cave/neuralnetwork2/loader/image/ImageWriter.java
+++ src/main/java/cave/neuralnetwork2/loader/image/ImageWriter.java
@@ -1,7 +1,10 @@
 package cave.neuralnetwork2.loader.image;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import cave.neuralnetwork2.loader.BatchData;
 import cave.neuralnetwork2.loader.Loader;
@@ -34,14 +37,27 @@ public class ImageWriter {
 		final String testImages = String.format("%s%s%s", directory, File.separator, "t10k-images-idx3-ubyte");
 		final String testLabels = String.format("%s%s%s", directory, File.separator, "t10k-labels-idx1-ubyte");
 
-		Loader trainLoader = new ImageLoader(trainImages, trainLabels, 32);
-		Loader testLoader = new ImageLoader(testImages, testLabels, 32);
+		int batchSize = 900;
+		
+		Loader trainLoader = new ImageLoader(trainImages, trainLabels, batchSize);
+		Loader testLoader = new ImageLoader(testImages, testLabels, batchSize);
 
 		trainLoader.open();
 		MetaData metaData = testLoader.open();
 
 		for (int i = 0; i < metaData.getNumberBatches(); i++) {
 			BatchData batchData = testLoader.readBatch();
+			
+			String montagePath = String.format("montage%d.jpg", i);
+			System.out.println("Writing " + montagePath);
+			
+			var montage = new BufferedImage(900, 900, BufferedImage.TYPE_BYTE_GRAY);
+			
+			try {
+				ImageIO.write(montage, "jpg", new File(montagePath));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 
 		trainLoader.close();
